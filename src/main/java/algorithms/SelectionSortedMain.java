4d773296a0b6ba656eff4d87d2fb9181e4190283
李澔澤 <a0909007892@gmail.com>
2024-05-16 21:33:35 +0800

    add selection sorted algorithms

--- src/main/java/algorithms/SelectionSortedMain.java
+++ src/main/java/algorithms/SelectionSortedMain.java
@@ -2,24 +2,26 @@ package algorithms;
 
 import java.util.Arrays;
 
-public class BubbleSortedMain {
+public class SelectionSortedMain {
 
     public static void main(String[] args) {
         int[] arr = {10, 100, 61, 22, 44, 67, 30, 90, 20, 34};
+        int cursor = 0;
         int count = 0;
         long start = System.currentTimeMillis();
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < (arr.length - i - 1); j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int k = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = k;
+            for (int j = arr.length - 1; j > i; j--) {
+                if (arr[i] > arr[j]) {
+                    int k = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = k;
                 }
                 count++;
             }
             System.out.println(Arrays.toString(arr));
         }
         long end = System.currentTimeMillis();
+        System.out.println("result: " + Arrays.toString(arr));
         System.out.println("cost: " + (end - start) + " ms, foreach count: " + count);
     }
 }
